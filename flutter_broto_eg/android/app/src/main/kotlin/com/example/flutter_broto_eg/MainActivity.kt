335cbb50ddaa89b5abc28b5a76aec8b2284ae90a
Abhinav R <abhinavrkhere@gmail.com>
2025-01-26 20:07:32 +0530

    app bar setting and homescreen setting are done

---
+++ flutter_broto_eg/android/app/src/main/kotlin/com/example/flutter_broto_eg/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_broto_eg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_broto_eg/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+      ),
+    );
+  }
+}
